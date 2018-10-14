4d8d7d64f9b4d707d22961442c3060520dd34401
Tomash <tpiljek@gmail.com>
2018-10-14 19:13:46 +0000

    Finished authentication with one error in Handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -9,6 +9,7 @@ use Illuminate\Auth\AuthenticationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Session\TokenMismatchException;
 use Illuminate\Validation\ValidationException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -104,6 +105,10 @@ class Handler extends ExceptionHandler
            }
         }
 
+        if ($exception instanceof TokenMismatchException) {
+            return redirect()->back()->withInput($request->input());
+        }
+
         //izvan produkcije
         if (config('app.debug')) {
             return parent::render($request, $exception);
@@ -123,6 +128,9 @@ class Handler extends ExceptionHandler
      */
     protected function unauthenticated($request, AuthenticationException $exception)
     {
+        if ($this->isFrontend($request)) {
+           return redirect()->guest('login');
+        }
         return $this->errorResponse('Unauthenticated', 401);
     }
 
@@ -137,6 +145,17 @@ class Handler extends ExceptionHandler
     {
        
         $errors = $e->validator->errors()->getMessages();
+
+        if ($this->isFrontend($request)) {
+            return $request->ajax() ? response()->json($error, 422) : redirect()->back()->withInput($request->input())->withErrors($errors);
+        }
+
         return $this->errorResponse($errors, 422);
+    }
+
+    protected function isFrontend($request)
+    {
+        return $request->acceptsHtml() && collect($request->route()->middleware())->contains('web');
+        //potrebno razriještiti problem kad se direktno upiše peapi.test/home kod ne redirekta nego daje 401 iz linije 134 što znači da ovaj kod od && ne radi kako treba.Vjerojatno zato što sam stavio u middleware group auth  u web rutingu
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
-<?php
+	<?php
+	
+/*PROPERTY;
 
-/*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
@@ -11,6 +12,25 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => ['web']], function () {
+    
+        // Authentication Routes...
+        $this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
+        $this->post('login', 'Auth\LoginController@login');
+        $this->post('logout', 'Auth\LoginController@logout')->name('logout');
+
+        // Password Reset Routes...
+        $this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+        $this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+        $this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+        $this->post('password/reset', 'Auth\ResetPasswordController@reset');
+
 });
+
+
+//Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', function() {
+	return view('welcome');
+})->middleware('guest');
